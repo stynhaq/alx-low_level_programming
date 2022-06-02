77d07fc2ac919a8d8e32dcb38d87e7b4fdb4eb91
stynhaq <stynhaq@live.com>
2022-06-01 20:14:51 -0700

    #6 Commit - Added Task 4,5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
 
+/** Code Begins
+*
+*
+*/
+int main(void){
+	char myStr[] = "\"Programming is like building a multilingual puzzle";
+	puts(myStr);
+	return 0;
+
+
+}
