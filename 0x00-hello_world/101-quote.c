5582838b17c77d2b4721b9418210a3e0f1683452
stynhaq <stynhaq@live.com>
2022-06-02 17:06:29 -0700

    #11 Commit - Created Task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -9,7 +10,7 @@
 
 int main(void)
 {
-	write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 
 }
