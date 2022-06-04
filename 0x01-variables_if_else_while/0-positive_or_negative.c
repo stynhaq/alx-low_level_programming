3cd01ca103f562e867646dc190275d139e457d0a
stynhaq <stynhaq@live.com>
2022-06-03 17:50:26 -0700

    #4 Commit - Added task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,6 +13,7 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -26,6 +27,5 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
-
-    return (0);
+	return (0);
 }
