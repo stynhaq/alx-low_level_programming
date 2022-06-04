b04c48acd2a36b7e1f552e3bcc7f8c2bd4669f17
stynhaq <stynhaq@live.com>
2022-06-03 20:20:58 -0700

    #8 Commit - Added tasks 101,102

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,33 +9,32 @@
 int main(void)
 {
 	int ch, n, m;
-
 	for (ch = 48; ch < 58; ch++)
 	{
 		for (n = 48; n < 58; n++)
-        {
-		for (m = 48; m < 58; m++)
 		{
-			if (ch == n || ch == m || n == m || n <= ch || m <= n)
+			for (m = 48; m < 58; m++)
 			{
+				if (ch == n || ch == m || n == m || n <= ch || m <= n)
+				{
+				}
+				else
+				{
+					putchar(ch);
+					putchar(n);
+					putchar(m);
+					if (ch == 55 && n == 56 && m == 57)
+					{
+					}
+					else
+					{
+						putchar(44);
+						putchar(' ');
+					}
+				}
 			}
-			else
-			{
-				putchar(ch);
-				putchar(n);
-				putchar(m);
-				if (ch == 55 && n == 56 && m == 57)
-                    			{
-                    			}
-                    		else
-                    			{
-                        			putchar(44);
-                        			putchar(' ');
-                   			}
-                	}
-            	}
-        }
-    }
+		}
+	}
     putchar('\n');
 
     return (0);
