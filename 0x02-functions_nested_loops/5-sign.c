59192a93a6f995068d4a4f875f5be93a577bd66c
stynhaq <stynhaq@live.com>
2022-06-08 06:22:11 -0700

    modified tasks 5 and 7'

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - print sign of number
- * @n: number to check
+ * @num: number to check
  * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
  */
 
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,16 +2,15 @@
 
 /**
  * print_last_digit - print last digit of integer
- * @n: integer argument
+ * @num: integer argument
  * Return: last digit of number
  */
 
-int print_last_digit(int n)
+int print_last_digit(int num)
 {
-	if (n < 0)
-		n *= -1;
+	if (num < 0)
+		num *= -1;
+	_putchar('0' + (num % 10));
 
-       	_putchar('0' + (n % 10));
-
-	return (n % 10);
+	return (num % 10);
 }
