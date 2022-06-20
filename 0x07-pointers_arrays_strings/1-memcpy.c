8c80d6d616e5fe80d89f21075dc0de72aef2d3c4
stynhaq <stynhaq@live.com>
2022-06-20 14:18:18 -0700

    Initial

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenate two strings
+ * @dest: string to be appended to
+ * @src: string to append
+ * @n: append n number of bytes(chars)
+ * Return: the concatenated string
+ */
+
+
+
+{
+
+
+}
