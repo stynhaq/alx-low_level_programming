82990234885119185cdfa2f27bd8d32be18f7cb5
stynhaq <stynhaq@live.com>
2022-06-22 21:14:02 -0700

    task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,10 +1,52 @@
 /**
- *  - a function 
- * @n: 
- * Return:  
+ * prime_check - recursive function to test primality
+ * @a: number to be checked for primality
+ * @b: number to be checked for primality
+ * Return:  0 if not prime or 1 if prime
  *
  */
 
-
+int prime_check(int a, int b)
 {
+	if (a == b)
+	{
+		return (1);
+	}
+
+	if (a % b == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (prime_check(a, b + 1));
+	}
+}
+
+
+/**
+ * is_prime_number - a function that checks if a number is a prime or not
+ * @n: number to be checked for primality
+ * Return:  0 if not prime or 1 if prime
+ *
+ */
+
+int is_prime_number(int n)
+{
+	int c = 2;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+
+	if (n == 2 || n == 3)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (prime_check(n, c));
+	}
 }
