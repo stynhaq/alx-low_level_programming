5e406783d3c254b462739c2f024911d728154cc4
stynhaq <stynhaq@live.com>
2022-06-22 20:35:13 -0700

    tough

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,27 @@
 /**
- *  - a function 
- * @n: 
- * Return:  
+ * _pow_recursion - a function that returns x raised to the power of y
+ * @x: Base
+ * @y: Exponent
+ * Return: the value of x raised to y
  *
  */
 
-
+int _pow_recursion(int x, int y)
 {
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+
+
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,44 @@
 /**
- *  - a function 
- * @n: 
- * Return:  
+ * _calc_square - a function that validates if a number is a square root
+ * @a: input number, check if the sqaure root is natural
+ * @b: number to test if is the root
+ * Return:  the natural square root of the number or -1
  *
  */
 
-
+int _calc_square(int a, int b)
 {
+	if (b * b == a)
+	{
+		return (b);
+	}
+
+	if (b * b > a)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_calc_square(a, b + 1));
+	}
+}
+
+
+/**
+ * _sqrt_recursion - a function that returns the natural square root of a num
+ * @n: input number to get it's square root
+ * Return:  the natural square root of the number
+ *
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n <= 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_calc_square(n, 0));
+	}
 }
