45a767a70a842eeeb9ca67b486468497ab0ce172
stynhaq <stynhaq@live.com>
2022-06-28 21:28:41 -0700

    Fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,7 +15,6 @@ char *_strdup(char *str)
 {
 	char *str2;
 	int i = 0;
-	int len = 0;
 
 	if (str == NULL)
 		return NULL;
--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+		len++, s++;
+
+	return (len);
+}
+
+
 /**
  * argstostr - function that concatenates all the arguments of your program
  *
@@ -12,4 +30,35 @@
 
 char *argstostr(int ac, char **av)
 {
+		char *s;
+	int len = 0, i, j, k = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		len += _strlen(av[i]);
+	}
+	len += (ac + 1);
+
+	s = malloc(len * sizeof(char));
+
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < _strlen(av[i]); j++)
+		{
+			s[k++] = av[i][j];
+		}
+		s[k++] = '\n';
+	}
+
+	return (s);
+
 }
