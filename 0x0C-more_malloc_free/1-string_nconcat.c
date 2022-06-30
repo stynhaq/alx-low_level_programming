d7a776dd3608522a1d54d61156aa5e2dcfb4cd3f
stynhaq <stynhaq@live.com>
2022-06-30 13:44:21 -0700

    Next Set

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * string_nconcat - a function that concatenates two strings
@@ -13,4 +14,42 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	int dest_index = 0;
+	int iter = 0;
+	char *dest2;
+	int n1 = n;
+
+	if (s1 == NULL)
+		s1 = "";
+
+        if (s2 == NULL)
+                s2 = "";
+
+	if (n1 < 0)
+		return (NULL);
+
+	if (strlen(s2) <= n)
+	{
+		n1 = strlen(s2);
+	}
+
+	dest2 = malloc(sizeof(*dest2) * (strlen(s1) + n1 + 1));
+		
+	if (dest2 == NULL)
+		return (NULL);
+		
+
+	for (dest_index = 0; s1[dest_index] != '\0'; dest_index++)
+	{
+		dest2[dest_index] = s1[dest_index];
+	}
+	
+	for (iter = 0; iter < n1; iter++)
+        {
+                dest2[dest_index + iter] = s2[iter];
+        }
+	dest2[dest_index + iter] = '\0';
+
+        return (dest2);
+	
 }
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,4 +12,17 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *mem;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	mem = calloc(nmemb, size);
+
+	if (mem == NULL)
+		return (NULL);
+
+	return (mem);
 }
