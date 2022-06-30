0d7e73646467a2d0cf30099e36584045953740df
stynhaq <stynhaq@live.com>
2022-06-30 13:51:47 -0700

    Fix_task_1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,9 +21,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-
-        if (s2 == NULL)
-                s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	if (n1 < 0)
 		return (NULL);
@@ -34,22 +33,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 	dest2 = malloc(sizeof(*dest2) * (strlen(s1) + n1 + 1));
-		
+
 	if (dest2 == NULL)
 		return (NULL);
-		
+
 
 	for (dest_index = 0; s1[dest_index] != '\0'; dest_index++)
 	{
 		dest2[dest_index] = s1[dest_index];
 	}
-	
+
 	for (iter = 0; iter < n1; iter++)
-        {
-                dest2[dest_index + iter] = s2[iter];
-        }
+	{
+		dest2[dest_index + iter] = s2[iter];
+	}
+
 	dest2[dest_index + iter] = '\0';
 
-        return (dest2);
-	
+	return (dest2);
 }
